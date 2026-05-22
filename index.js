f1f29239bc71a4cdf192abdcc2ba6ed663853f84
Atziry Cuxil <cuxilatziry@gmail.com>
2026-05-22 17:14:39 -0600

    Agrego una parte del DOM

--- index.js
+++ index.js
@@ -1,20 +1,8 @@
-let info = [];
-let btn = document.querySelectorAll('.btn')
-let input = document.querySelector('#pantalla')
+let botones = document.querySelectorAll('.btn')
+let iput = document.querySelector('#pantalla')
 
-btn.addEventListener('click', (event) => {
-    let pintar = event.target.id
-    calculadora.forEach(item => item.id == pintar ? valor = item.value : item)
-    input.value = valor
+botones.forEach(btn => {
+    btn.addEventListener('click', (event) => {
+        input.value += event.target.textContent
+    })
 })
-
-
-
-// let botones = document.querySelectorAll('.btn')
-// let iput = document.querySelector('#pantalla')
-
-// botones.forEach(btn => {
-//     btn.addEventListener('click', (event) => {
-//         input.value += event.target.textContent
-//     })
-// })
