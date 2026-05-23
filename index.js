cbac0a3a0a1875114e1c5a3772851c2cb44bfb2a
Atziry Cuxil <cuxilatziry@gmail.com>
2026-05-22 18:19:59 -0600

    Agrego condicionales

--- index.js
+++ index.js
@@ -1,8 +1,27 @@
 let botones = document.querySelectorAll('.btn')
-let iput = document.querySelector('#pantalla')
+let input = document.querySelector('#pantalla')
 
 botones.forEach(btn => {
     btn.addEventListener('click', (event) => {
-        input.value += event.target.textContent
+
+        // input.value += event.target.textContent
+
+        let signos = "/*-+.%"
+
+        if (input.value.length == 0) {
+            if (!signos.includes(event.target.textContent)) {
+                input.value += event.target.textContent
+            }
+        } else {
+
+            if (signos.includes(event.target.textContent)) {
+                if (input.value.length[input.value.length - 1]) {
+                    input.value.replace(input.value[input.value.length - 1], event.target.textContent)
+                }
+            } else {
+                input.value += event.target.textContent
+            }
+        }
     })
 })
+
